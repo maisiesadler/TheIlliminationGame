264184356433d37eda1e3fbc9a2ca7f30b56ad12
Maisie <maisiesadler@hotmail.com>
2020-03-29 15:36:44 +0100

    use endpoint

--- database/main.go
+++ database/main.go
@@ -33,6 +33,7 @@ func Connect() bool {
 
 func getConnectionString() (string, error) {
 	region := "eu-west-2"
+	endpoint := "ssm.eu-west-2.amazonaws.com"
 	sess, err := session.NewSessionWithOptions(session.Options{
 		Config:            aws.Config{Region: aws.String(region)},
 		SharedConfigState: session.SharedConfigEnable,
@@ -42,7 +43,7 @@ func getConnectionString() (string, error) {
 		return "", err
 	}
 
-	ssmsvc := ssm.New(sess, aws.NewConfig().WithRegion(region))
+	ssmsvc := ssm.New(sess, aws.NewConfig().WithRegion(region).WithEndpoint(endpoint))
 	keyname := "mongodb"
 	withDecryption := true
 
