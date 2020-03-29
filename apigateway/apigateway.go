5f045eb35403f0d00b8e35acaf839c6fc7083492
Maisie <maisiesadler@hotmail.com>
2020-03-29 13:25:19 +0100

    add unsuccessful response

--- apigateway/apigateway.go
+++ apigateway/apigateway.go
@@ -25,3 +25,13 @@ func ResponseSuccessfulString(body string) events.APIGatewayProxyResponse {
 	resp.StatusCode = 200
 	return resp
 }
+
+// ResponseUnsuccessfulString return response for API Gateway that allows cors
+func ResponseUnsuccessfulString(statusCode int, body string) events.APIGatewayProxyResponse {
+	resp := events.APIGatewayProxyResponse{Headers: make(map[string]string)}
+	resp.Headers["Access-Control-Allow-Origin"] = "*"
+	resp.Headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization,dfd-auth"
+	resp.Body = body
+	resp.StatusCode = statusCode
+	return resp
+}
