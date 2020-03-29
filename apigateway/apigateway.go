0366248482bcfda4cc5ab258ae5bd45a2d1f7ace
Maisie <maisiesadler@hotmail.com>
2020-03-29 12:24:34 +0100

    Add overload

--- apigateway/apigateway.go
+++ apigateway/apigateway.go
@@ -1,11 +1,23 @@
 package apigateway
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 )
 
 // ResponseSuccessful returns a 200 response for API Gateway that allows cors
-func ResponseSuccessful(body string) events.APIGatewayProxyResponse {
+func ResponseSuccessful(v interface{}) events.APIGatewayProxyResponse {
+	b, err := json.Marshal(v)
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500}
+	}
+
+	return ResponseSuccessfulString(string(b))
+}
+
+// ResponseSuccessfulString returns a 200 response for API Gateway that allows cors
+func ResponseSuccessfulString(body string) events.APIGatewayProxyResponse {
 	resp := events.APIGatewayProxyResponse{Headers: make(map[string]string)}
 	resp.Headers["Access-Control-Allow-Origin"] = "*"
 	resp.Headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization,dfd-auth"
