e7e1bbf4c7578fdb1625092614e0fc2b3fe810b4
Maisie <maisiesadler@hotmail.com>
2020-04-09 19:21:15 +0100

    override collection using function

--- database/testoverrides.go
+++ database/testoverrides.go
@@ -1,17 +1,19 @@
 package database
 
-var overrides map[string]ICollection
+var createOverrideFn func(string) ICollection
 
 func tryGetOverrideFor(database string, collection string) (ICollection, bool) {
 	key := database + "_" + collection
 
-	val, ok := overrides[key]
-	return val, ok
+	if createOverrideFn != nil {
+		override := createOverrideFn(key)
+		return override, true
+	}
+
+	return nil, false
 }
 
 // SetOverride allows an override ICollection to be set for testing
-func SetOverride(database string, collection string, coll ICollection) {
-	key := database + "_" + collection
-
-	overrides[key] = coll
+func SetOverride(overrideFn func(string) ICollection) {
+	createOverrideFn = overrideFn
 }
