339e1c5b86f3580f34dd8cfddfe71afef5f7c33f
Maisie <maisiesadler@hotmail.com>
2021-01-17 17:21:51 +0000

    Add model for user option

--- models/userview.go
+++ models/userview.go
@@ -10,3 +10,18 @@ type UserView struct {
 	Username string              `json:"username"`
 	Nickname string              `json:"nickname"`
 }
+
+// UserOptions represents the users options
+type UserOptions struct {
+	ID       *primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	UserID   primitive.ObjectID  `json:"userId"`
+	Nickname string              `json:"nickname"`
+	Option   UserOption          `json:"option"`
+}
+
+// UserOption is an option used in UserOptions
+type UserOption struct {
+	Name        string
+	Description string
+	Link        string
+}
