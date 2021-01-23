f92998d91169a79eafbb48ffe620349cade5352a
Maisie <maisiesadler@hotmail.com>
2021-01-23 10:22:56 +0000

    gamesetupids

--- models/userview.go
+++ models/userview.go
@@ -13,12 +13,11 @@ type UserView struct {
 
 // UserOption represents the users options
 type UserOption struct {
-	ID          *primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	UserID      primitive.ObjectID  `json:"userId"`
-	Name        string              `json:"name"`
-	Description string              `json:"description"`
-	Link        string              `json:"link"`
-	Tags        []string            `json:"tags"`
-	GameSetupID primitive.ObjectID  `json:"gameSetupId"`
-	IsOverride  bool                `json:"isOverride"`
+	ID           *primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	UserID       primitive.ObjectID  `json:"userId"`
+	Name         string              `json:"name"`
+	Description  string              `json:"description"`
+	Link         string              `json:"link"`
+	Tags         []string            `json:"tags"`
+	GameSetupIDs primitive.ObjectID  `json:"gameSetupIds"`
 }
